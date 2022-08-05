feb7bfbc99ebe56922155ba10576633f2df5c5ac
Avani14 <trivediavani14@gmail.com>
2022-08-05 15:30:52 +0530

    Use of @PutMapping annotation to pass first name as @Pathvariable and last name of a person as @RequestParam and display them on localhost:8080/hello/Avani/?last_name=Trivedi.

--- src/main/java/com/bridgelabz/messagingapp/controller/HelloWorld.java
+++ src/main/java/com/bridgelabz/messagingapp/controller/HelloWorld.java
@@ -10,8 +10,13 @@ public class HelloWorld {
     public String helloWorld(@PathVariable String name){
         return "Hello "+name;
     }
+
     @PostMapping("post")
     public String creatingAPerson(@RequestBody Person person){
         return "Hello "+person.getFirst_name()+" "+person.getLast_name();
+    }
+    @PutMapping("put/{first_name}")
+    public String updatingPerson(@PathVariable String first_name,@RequestParam String last_name){
+        return "Hello "+first_name+" "+last_name;
     }
 }
